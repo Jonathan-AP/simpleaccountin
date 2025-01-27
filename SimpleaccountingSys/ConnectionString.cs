9f1f08c3924ae460c15057c429313e83e5d3da94
Jonathan-AP <157302609+Jonathan-AP@users.noreply.github.com>
2025-01-27 11:33:14 -0500

    Add files via upload

---
+++ SimpleaccountingSys/ConnectionString.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace College_Management_System
+{
+    class ConnectionString
+    {
+        public string DBConn = @"Data Source=DESKTOP-6LGRR55;Initial Catalog=SimpleAccount;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+       //Data Source=SOLA;Initial Catalog=CMS_DB.MDF;Integrated Security=True;Connect Timeout=30
+        //Data Source=SOLA\SOLA;Initial Catalog=CMS_DB.MDF;Integrated Security=True
+    }
+}
